d3c84fb420a5a746adfd1da3794ddb90dda1aa76
unino <phuongtrang06@gmail.com>
2024-11-22 11:10:44 +0700

    update pagination, register, login

--- saleapp/app/index.py
+++ saleapp/app/index.py
@@ -1,17 +1,73 @@
-from flask import render_template, request
+import math
+
+from flask import render_template, request, redirect
 import dao
-from app import app
+from app import app, login
+from flask_login import login_user, logout_user
 
 
 @app.route("/")
 def index():
-    cates = dao.load_categories()
-
     cate_id = request.args.get('category_id')
     kw = request.args.get('kw')
-    prods = dao.load_products(cate_id=cate_id, kw=kw)
+    page = request.args.get('page', 1)
+    prods = dao.load_products(cate_id=cate_id, kw=kw, page=int(page))
 
-    return render_template('index.html', categories=cates, products=prods)
+    page_size = app.config['PAGE_SIZE']
+    total = dao.count_products()
+    return render_template('index.html', products=prods, pages=math.ceil(total/page_size))
+
+
+@app.route("/login", methods=['get', 'post'])
+def login_process():
+    if request.method.__eq__('POST'):
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        u = dao.auth_user(username=username, password=password)
+        if u:
+            login_user(u)
+            return redirect('/')
+
+    return render_template('login.html')
+
+
+@app.route("/logout")
+def logout_process():
+    logout_user()
+    return redirect('/login')
+
+
+@app.route("/register", methods=['get', 'post'])
+def register_process():
+    err_msg = None
+    if request.method.__eq__('POST'):
+        password = request.form.get('password')
+        confirm = request.form.get('confirm')
+
+        if password.__eq__(confirm):
+            data = request.form.copy()
+            del data['confirm']
+
+            avatar = request.files.get('avatar')
+            dao.add_user(avatar=avatar, **data)
+            return redirect('/login')
+        else:
+            err_msg = 'Mật khẩu KHÔNG khớp!'
+
+    return render_template('register.html', err_msg=err_msg)
+
+
+@app.context_processor
+def common_context_params():
+    return {
+        'categories': dao.load_categories()
+    }
+
+
+@login.user_loader
+def get_user_by_id(user_id):
+    return dao.get_user_by_id(user_id)
 
 
 if __name__ == '__main__':
